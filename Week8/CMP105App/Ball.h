53e3b66e2d01ab367d79f629019bf25322785e02
1900608 Elliot Stevenson <1900608@uad.ac.uk>
2020-03-21 21:49:25 +0000

    Create Ball.h

---
+++ Week8/CMP105App/Ball.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Framework/GameObject.h"
+class Ball: public GameObject
+{
+public:
+	Ball();
+	~Ball();
+
+	void update(float dt) override;
+	void collisionResponse(GameObject* collider) override;
+};
+
